dab6cd5abfd5273ce6db8174de9441ca41997e34
Rusty Eddy <rusty@rustyeddy.com>
2019-01-13 19:46:04 -0800

    Both motors work

--- drive/tank.ino
+++ drive/tank.ino
@@ -5,7 +5,7 @@
 #include <AFMotor.h>
 
 AF_DCMotor m1(1);
-// AF_DCMotor m2(2);
+AF_DCMotor m2(2);
 
 // The tank can only be going in one direction
 int direction = FORWARD;
@@ -32,41 +32,18 @@ void reverse(AF_DCMotor *m, int speed) {
 	m->run(BACKWARD);
 }
 
-void back_and_forth_loop() {
-
-	Serial.print("Start in ");
-	Serial.println(direction);
-
-  // turn on motor
-	forward(&m1, 300);
-	//forward(&m2, 300);
-
-	delay(2000);
-
-  // turn on motor
-	//forward(&m2, 300);
-	reverse(&m1, 300);
-	delay(2000);
-	
-  m1.run(RELEASE);
-  //m2.run(RELEASE);
-
-	Serial.println("released  ... ");
-}
-
-void spin_right_loop() {
-	Serial.println("Spinning wheel goes spinning around");
-	forward(&m1, 300);
-	//forward(&m2, 300);
-
-	delay(2000);
-	//m2.run(RELEASE);
-	m1.run(RELEASE);
-}
-
 void loop() {
+
+	m1.setSpeed(100);
+	m2.setSpeed(200);
+	m1.run(FORWARD);
+	m2.run(FORWARD);
+
+	delay(2000);
+	m1.run(RELEASE);
+	m2.run(RELEASE);
 	//back_and_forth_loop();
-	spin_right_loop();
+	//spin_right_loop();
 }
 
 
